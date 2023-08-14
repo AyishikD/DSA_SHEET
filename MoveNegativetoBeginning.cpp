9f821308b6e07de35805da1a1e3aab4dd23c13f3
AyishikD <99983449+AyishikD@users.noreply.github.com>
2023-08-14 21:03:17 +0530

    Create MoveNegativetoBeginning.cpp

---
+++ MoveNegativetoBeginning.cpp
@@ -0,0 +1,6 @@
+void move(vector<int>& arr){
+  sort(arr.begin(),arr.end());
+}
+
+// however for reducing time constraints, other algo can be used
+//https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/ --link
