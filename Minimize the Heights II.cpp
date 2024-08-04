08934de2bf122874438b05bc5839e7fd9e84aaa7
Ayishik Das <99983449+AyishikD@users.noreply.github.com>
2024-08-05 02:03:11 +0530

    Create Minimize the Heights II.cpp

---
+++ Minimize the Heights II.cpp
@@ -0,0 +1,15 @@
+int getMinDiff(int arr[], int n, int k) {
+        // code here
+        sort(arr,arr+n);
+        int m=arr[n-1]-arr[0];
+        int c,d;
+        for(int i=0;i<n;i++){
+            c=min(arr[i]-k,arr[0]+k);
+            d=max(arr[i-1]+k,arr[n-1]-k);
+            if(c<0)
+                continue;
+            m=min(m,d-c);
+        }
+        return m;
+    }
+// Good one I encountered vvip
